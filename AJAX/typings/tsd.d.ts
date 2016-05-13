49caf8db8492765a4af02783284259ef83e37833
Shahzaibanalyst <shahzaibanalyst@outlook.com>
2016-05-13 17:22:28 +0500

    AJAX HTTP Request with RxJS

---
+++ AJAX/typings/tsd.d.ts
@@ -0,0 +1,6 @@
+///<reference path="./express-session/express-session.d.ts"/>
+///<reference path="./express/express.d.ts"/>
+///<reference path="./node/node.d.ts"/>
+///<reference path="./body-parser/body-parser.d.ts"/>
+///<reference path="./mongoose/mongoose.d.ts"/>
+///<reference path="./rx/rx.d.ts"/>
